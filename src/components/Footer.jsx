12050c3d719b0cceb2a41683e424c9e6b6a7c253
Ariel Caminos <caminosariel@gmail.com>
2024-09-28 19:39:11 -0300

    test show image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="flex justify-center text-teal-600 sm:justify-start">
-            <img alt="" src="/public/webTools.png" className='h-7'/>
+            <img alt="" src="/webtools.png" className='h-7'/>
           </div>
           <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">Copyright &copy; 2022. All rights reserved.</p>
         </div>
